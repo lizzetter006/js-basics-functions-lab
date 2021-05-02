8e249a08f67ff7839f3fa42f8b375847c3ac446d
Lizzette <Lizzetter00@gmail.com>
2021-05-01 20:09:59 -0400

    commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (){
+ return "8 blocks";
+ 
+}
+function distanceFromHqInFeet (){
+ distanceFromHqInBlocks(8);
+ return 2,112;
+
+}
+function distanceTravelledInFeet (){
+    return 8 * 264;
+}
+
+function calculatesFarePrice ();{
+   return (400 - 2,112);
+    if(Feet < 2,500)
+    return 'cannot travel that far';
+     if (Feet > 2,500)
+     return 'can travel up to 2500';
+}
+console.log(calculatesFarePrice(1,712));
